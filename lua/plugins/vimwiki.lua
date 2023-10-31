d4bc5aa545bcc7b74a1a6209d1abb98356d01102
Tim Morgan <tim@timmorgan.org>
2023-10-31 15:11:30 -0500

    Don't activate vimwiki outside vimwiki folder

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,6 +1,7 @@
 return {
   "vimwiki/vimwiki",
-  config = function()
+  init = function()
     vim.g.vimwiki_map_prefix = "<leader>W"
+    vim.g.vimwiki_global_ext = 0 -- only activate in the ~/vimwiki folder
   end,
 }
