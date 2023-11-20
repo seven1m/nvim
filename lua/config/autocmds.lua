179b031c7b7e9d8ba47a2a25348033a636ecf430
Tim Morgan <tim@timmorgan.org>
2023-11-19 22:38:52 -0600

    Don't dedent when { is pressed

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,4 +3,4 @@
 -- Add any additional autocmds here
 
 -- https://github.com/nvim-treesitter/nvim-treesitter/issues/3363#issuecomment-1538607633
-vim.cmd("autocmd FileType ruby setlocal indentkeys-=.")
+vim.cmd("autocmd FileType ruby setlocal indentkeys-=. indentkeys-=0{")
