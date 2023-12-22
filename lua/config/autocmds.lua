0e80c22af530be87fd25d2aac4fa322290ba885d
Tim Morgan <tim@timmorgan.org>
2023-12-21 21:37:50 -0600

    Stop hiding characters in markdown files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,3 +4,6 @@
 
 -- https://github.com/nvim-treesitter/nvim-treesitter/issues/3363#issuecomment-1538607633
 vim.cmd("autocmd FileType ruby setlocal indentkeys-=. indentkeys-=0{")
+
+-- https://vi.stackexchange.com/questions/6512/neovim-hides-the-chars-when-editing-markdown
+vim.cmd("autocmd FileType markdown set conceallevel=0")
