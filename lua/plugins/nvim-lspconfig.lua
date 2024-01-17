7c40a069e544a1b6f800639c1e87d9e7169b9de5
Tim Morgan <tim@timmorgan.org>
2024-01-17 11:06:54 -0600

    Add another note for ruby-lsp debug

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -8,6 +8,7 @@ return {
       ruby_ls = {
         -- NOTE TO SELF: If you get weird errors that don't go away when you save the file,
         -- you probably need to add 'rubocop' to the gemfile of the project!
+        -- And if nothing works at all, try `gem install ruby-lsp`.
         mason = false, -- I install myself please
       },
       syntax_tree = {
