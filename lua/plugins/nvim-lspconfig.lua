8d3da8c10cea098e3e392b26a8935a3bd6821f77
Tim Morgan <tim@timmorgan.org>
2024-05-06 13:36:42 -0500

    Try to fix ruby-lsp again

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,12 +4,19 @@ return {
     diagnostics = {
       underline = false,
     },
+    --inlay_hints = {
+    --enabled = true,
+    --},
+    --codelens = {
+    --enabled = true,
+    --},
     servers = {
       ruby_lsp = {
         -- NOTE TO SELF: If you get weird errors that don't go away when you save the file,
         -- you probably need to add 'rubocop' to the gemfile of the project!
-        -- And if nothing works at all, try `gem install ruby-lsp`.
+        -- And if nothing works at all, try `gem install ruby-lsp` followed by `ruby-lsp` and Ctrl-C.
         mason = false, -- I install myself please
+        cmd_env = { BUNDLE_GEMFILE = ".ruby-lsp/Gemfile" },
       },
       syntax_tree = {
         mason = false, -- I install myself please
