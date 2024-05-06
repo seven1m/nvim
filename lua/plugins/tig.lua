09fa4afa48a91e6bca8be865086da32a73985439
Tim Morgan <tim@timmorgan.org>
2024-05-06 13:36:47 -0500

    Add tig

---
+++ lua/plugins/tig.lua
@@ -0,0 +1,37 @@
+return {
+  "codeindulgence/vim-tig",
+  init = function()
+    local wk = require("which-key")
+    wk.register({ ["<leader>ct"] = { name = "+tig" } })
+  end,
+  keys = {
+    {
+      "<leader>ctb",
+      function()
+        vim.cmd("Tig blame " .. vim.fn.expand("%"))
+      end,
+      desc = "Tig blame current file",
+    },
+    {
+      "<leader>cts",
+      function()
+        vim.cmd("Tig status")
+      end,
+      desc = "Tig status",
+    },
+    {
+      "<leader>ctl",
+      function()
+        vim.cmd("Tig log")
+      end,
+      desc = "Tig log",
+    },
+    {
+      "<leader>ctL",
+      function()
+        vim.cmd("Tig log " .. vim.fn.expand("%"))
+      end,
+      desc = "Tig log current file",
+    },
+  },
+}
