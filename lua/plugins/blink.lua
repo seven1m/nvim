7a333ed8a16b39e814dd005160f04c3cf24a8a20
Tim Morgan <tim@timmorgan.org>
2024-12-19 09:02:15 -0600

    Disable enter-for-completion in blink

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,10 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      -- LazyVim sets this to the "enter" preset, and I don't like that.
+      -- I use Ctrl-Y to select the completion. :-)
+      preset = "default",
+    },
+  },
+}
