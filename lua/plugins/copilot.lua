8d9a7ee4ab48a075e6db677b46ade265da554c19
Tim Morgan <tim@timmorgan.org>
2025-05-20 23:06:31 -0500

    Fix copilot_node_command for linux

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,13 @@
+local copilot_node_command = "node"
+local homebrew_node_path = "/opt/homebrew/bin/node"
+if vim.fn.filereadable(homebrew_node_path) == 1 then
+  copilot_node_command = homebrew_node_path
+end
+
 return {
   "zbirenbaum/copilot.lua",
   opts = {
     copilot_model = "gpt-4o-copilot",
-    copilot_node_command = "/opt/homebrew/bin/node",
+    copilot_node_command = copilot_node_command,
   },
 }
