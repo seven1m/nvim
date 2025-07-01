6c00aac4f16441b0378d713b8e95b3de7ecabb33
Tim Morgan <tim@timmorgan.org>
2025-07-01 10:31:16 -0500

    Minor formatting change

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -40,16 +40,16 @@ return {
       },
       syntax_tree = {
         mason = false,
+        -- You need to install syntax_tree
+        --
+        --     gem install syntax_tree
+        --
         cmd_env = {
           -- Neovim is overwriting GEM_HOME somehow.
           -- Setting it back to the value set by Devbox,
           -- which is also stored by Devbox in RUBY_CONFDIR.
           GEM_HOME = vim.env.RUBY_CONFDIR,
         },
-        -- You need to install syntax_tree
-        --
-        --     gem install syntax_tree
-        --
       },
       solargraph = {
         -- I hate solargraph. :-(
