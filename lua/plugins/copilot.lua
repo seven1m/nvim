54c5da0b95cc8397267075823e06dda113749bb8
Tim Morgan <tim@timmorgan.org>
2025-07-15 10:47:24 -0700

    Don't set copilot model

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
   "zbirenbaum/copilot.lua",
   opts = {
-    copilot_model = "gpt-4o-copilot",
+    -- copilot_model = "gpt-4o-copilot",
     copilot_node_command = "/opt/homebrew/bin/node",
   },
 }
