e300d388af284d298cf129bef42352dfd2b3745d
Tim Morgan <tim@timmorgan.org>
2025-10-01 11:12:44 -0500

    Fix node path for copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,22 @@
+local function path_exists(path)
+  local f = io.open(path, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
+local node_path = "/opt/homebrew/bin/node"
+if not path_exists(node_path) then
+  node_path = "node"
+end
+
 return {
   "zbirenbaum/copilot.lua",
   opts = {
     -- copilot_model = "gpt-4o-copilot",
-    copilot_node_command = "/opt/homebrew/bin/node",
+    copilot_node_command = node_path,
   },
 }
